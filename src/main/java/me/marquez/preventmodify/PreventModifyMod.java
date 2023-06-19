d935d4fb23ca146adf8c3499e70f646bcddef350
SkyAsa2256 <asa2256@icyonline.kr>
2023-06-19 13:26:09 +0900

    Ported 1.16.5 * Player cannot add or modify a multiplayer server, and if you try, you will be returned to MainMenu

--- src/main/java/me/marquez/preventmodify/PreventModifyMod.java
+++ src/main/java/me/marquez/preventmodify/PreventModifyMod.java
@@ -1,18 +1,22 @@
 package me.marquez.preventmodify;
 
-import com.mojang.logging.LogUtils;
-import net.minecraft.client.gui.screens.EditServerScreen;
-import net.minecraftforge.client.event.ScreenEvent;
+
+import com.pixelmonmod.pixelmon.client.gui.override.multiplayer.CustomAddServerScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.AddServerScreen;
+import net.minecraft.client.gui.screen.MainMenuScreen;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.slf4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod(PreventModifyMod.MODID)
 public class PreventModifyMod
 {
     public static final String MODID = "preventmodify";
-    private static final Logger LOGGER = LogUtils.getLogger();
+    private static final Logger LOGGER = LogManager.getLogger();
 
     public PreventModifyMod()
     {
@@ -20,10 +24,16 @@ public class PreventModifyMod
     }
 
     @SubscribeEvent
-    public void onGuiOpen(ScreenEvent e) {
-        if (e.getScreen() instanceof EditServerScreen) {
+    public void onGuiOpen(GuiScreenEvent.DrawScreenEvent.Pre e) {
+        // Pixelmon
+        if (e.getGui() instanceof CustomAddServerScreen) {
             e.setCanceled(true);
+            Minecraft.getInstance().setScreen(new MainMenuScreen());
         }
+        // Vanilla
+//        if (e.getGui() instanceof AddServerScreen) {
+//            e.setCanceled(true);
+//        }
 
     }
 }
